643b4472a15d541aa2fbb0ccbc57ed9e8e19af52
daishuguang <daishuguang4461@126.com>
2014-07-14 17:47:20 +0800

    commit

--- Aihunqin/src/com/aihunqin/crazy/FragmentInvitation.java
+++ Aihunqin/src/com/aihunqin/crazy/FragmentInvitation.java
@@ -3,6 +3,7 @@ package com.aihunqin.crazy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager.OnBackStackChangedListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
