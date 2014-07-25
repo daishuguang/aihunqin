b2ff27a02bac36020a955000c80ace1d56cef22d
daishuguang <daishuguang4461@126.com>
2014-07-25 17:45:24 +0800

    commit

--- Aihunqin/src/com/aihunqin/fragment/FragmentMain.java
+++ Aihunqin/src/com/aihunqin/fragment/FragmentMain.java
@@ -1,6 +1,17 @@
 package com.aihunqin.fragment;
 
-import com.example.aihunqin.R;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,10 +21,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.aihunqin.R;
 
 public class FragmentMain extends Fragment {
 	OnClickListener settingnameListener = new OnClickListener() {
@@ -83,6 +94,44 @@ public class FragmentMain extends Fragment {
 		// Setting bride name
 		TextView bride = (TextView) getView().findViewById(R.id.bridename);
 		bride.setOnClickListener(settingnameListener);
+
+		/** Read data from xml */
+		String re = "";
+		DocumentBuilderFactory documentBuilderFactory;
+		DocumentBuilder documentBuilder;
+		Document document;
+		documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		try {
+			documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			// xml文件在默认目录
+			document = documentBuilder.parse(getActivity().openFileInput(
+					"namesetting.xml"));
+			Element root = document.getDocumentElement();
+			NodeList nodeList = root.getElementsByTagName("bridename");
+			Node nd = nodeList.item(0);
+			re = nd.getFirstChild().getNodeValue();
+			bride.setText(re);
+			nodeList = root.getElementsByTagName("bridegroomname");
+			nd = nodeList.item(0);
+			re = nd.getFirstChild().getNodeValue();
+			bridegroom.setText(re);
+		} catch (ParserConfigurationException e) {
+
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		} catch (SAXException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		} finally {
+			document = null;
+			documentBuilder = null;
+			documentBuilderFactory = null;
+		}
 
 	}
 }
--- Aihunqin/src/com/aihunqin/fragment/FragmentSettingName.java
+++ Aihunqin/src/com/aihunqin/fragment/FragmentSettingName.java
@@ -1,18 +1,41 @@
 package com.aihunqin.fragment;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.xmlpull.v1.XmlSerializer;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.aihunqin.R;
 
 public class FragmentSettingName extends Fragment {
+	EditText bridegroom;
+	EditText bride;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -20,9 +43,51 @@ public class FragmentSettingName extends Fragment {
 				.inflate(R.layout.fragment_settingname, container, false);
 	}
 
+	void readFromXML() {
+		String str = "";
+		DocumentBuilderFactory documentBuilderFactory;
+		DocumentBuilder documentBuilder;
+		Document document;
+		documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		try {
+			documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			// xml文件在默认目录
+			document = documentBuilder.parse(getActivity().openFileInput(
+					"namesetting.xml"));
+			Element root = document.getDocumentElement();
+			NodeList nodeList = root.getElementsByTagName("bridename");
+			Node nd = nodeList.item(0);
+			str = nd.getFirstChild().getNodeValue();
+			bride.setText(str);
+			nodeList = root.getElementsByTagName("bridegroomname");
+			nd = nodeList.item(0);
+			str = nd.getFirstChild().getNodeValue();
+			bridegroom.setText(str);
+		} catch (ParserConfigurationException e) {
+
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		} catch (SAXException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		} finally {
+			document = null;
+			documentBuilder = null;
+			documentBuilderFactory = null;
+		}
+	}
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
+		bride = (EditText) getView().findViewById(R.id.bridename);
+		bridegroom = (EditText) getView().findViewById(R.id.bridegroomname);
+		readFromXML();
 		((TextView) getView().findViewById(R.id.titleTv)).setText("照片和名字");
 		((TextView) getView().findViewById(R.id.back))
 				.setVisibility(View.VISIBLE);
@@ -51,7 +116,52 @@ public class FragmentSettingName extends Fragment {
 
 					@Override
 					public void onClick(View v) {
-						
+						String bridename = bride.getText().toString();
+						String bridegroomname = bridegroom.getText().toString();
+
+						String FILENAME = "namesetting.xml";
+
+						try {
+							XmlSerializer serializer = Xml.newSerializer();
+							StringWriter writer = new StringWriter();
+							serializer.setOutput(writer);
+							serializer.startDocument("utf-8", true);
+							serializer.startTag("", "user");
+							serializer.startTag("", "bridename");
+							serializer.text(bridename);
+							serializer.endTag("", "bridename");
+
+							serializer.startTag("", "bridegroomname");
+							serializer.text(bridegroomname);
+							serializer.endTag("", "bridegroomname");
+
+							serializer.startTag("", "pic");
+							serializer.text("");
+							serializer.endTag("", "pic");
+
+							serializer.endTag("", "user");
+							serializer.endDocument();
+							OutputStream out = getActivity().openFileOutput(
+									FILENAME, Context.MODE_PRIVATE);
+							OutputStreamWriter outWriter = new OutputStreamWriter(
+									out);
+							outWriter.write(writer.toString());
+							outWriter.close();
+							out.close();
+						} catch (FileNotFoundException e) {
+
+							e.printStackTrace();
+						} catch (IOException e) {
+
+							e.printStackTrace();
+						}
+
+						FragmentManager fragmentmanager = getActivity()
+								.getSupportFragmentManager();
+						Fragment fragment = new FragmentMain();
+						fragmentmanager.beginTransaction()
+								.replace(R.id.fragment_container, fragment)
+								.commit();
 					}
 				});
 	}
