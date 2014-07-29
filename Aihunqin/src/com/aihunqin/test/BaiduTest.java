81e0bc93a53d7bbf42710b3ce33632407c1dcece
daishuguang <daishuguang4461@126.com>
2014-07-30 01:45:44 +0800

    commit

--- Aihunqin/src/com/aihunqin/test/BaiduTest.java
+++ Aihunqin/src/com/aihunqin/test/BaiduTest.java
@@ -3,17 +3,21 @@ package com.aihunqin.test;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.MapView;
 import com.example.aihunqin.R;
 
 public class BaiduTest extends Activity {
+
 	MapView mMapView = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
-
+		// 在使用SDK各组件之前初始化context信息，传入ApplicationContext
+		// 注意该方法要在setContentView方法之前实现
+		SDKInitializer.initialize(getApplicationContext());
 		setContentView(R.layout.baidutest);
 		// 获取地图控件引用
 		mMapView = (MapView) findViewById(R.id.bmapView);
@@ -24,5 +28,21 @@ public class BaiduTest extends Activity {
 
 		super.onDestroy();
 		// 在activity执行onDestroy时执行mMapView.onDestroy(),实现地图生命周期管理
+		mMapView.onDestroy();
+	}
+
+	@Override
+	protected void onResume() {
+
+		super.onResume();
+		mMapView.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+
+		super.onPause();
+		// 在activity执行onPause时执行mMapView.onPause(),实现地图生命周期管理
+		mMapView.onPause();
 	}
 }
