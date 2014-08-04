4d5285ca075493eae8c8c9945e81b977cfef60e1
daishuguang <daishuguang4461@126.com>
2014-08-04 11:33:13 +0800

    commit

--- CommonComponent/src/com/common/ui/BaseAdapterTest.java
+++ CommonComponent/src/com/common/ui/BaseAdapterTest.java
@@ -3,6 +3,7 @@ package com.common.ui;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -26,19 +27,29 @@ public class BaseAdapterTest extends Activity {
 
 			@Override
 			public View getView(int position, View convertView, ViewGroup parent) {
-				// 创建一个LinearLayout,并向其中添加两个组件
-				LinearLayout line = new LinearLayout(getApplicationContext());
-				line.setOrientation(LinearLayout.HORIZONTAL);
-				ImageView image = new ImageView(getApplicationContext());
-				image.setImageResource(R.drawable.tiger);
-				TextView text = new TextView(getApplicationContext());
-				text.setText("第" + (position + 1) + "个列表项");
-				text.setTextSize(20);
-				text.setTextColor(Color.RED);
-				line.addView(image);
-				line.addView(text);
-				// 返回LinearLayout实例
-				return line;
+				// // 创建一个LinearLayout,并向其中添加两个组件
+				// LinearLayout line = new
+				// LinearLayout(getApplicationContext());
+				// line.setOrientation(LinearLayout.HORIZONTAL);
+				// ImageView image = new ImageView(getApplicationContext());
+				// image.setImageResource(R.drawable.tiger);
+				// TextView text = new TextView(getApplicationContext());
+				// text.setText("第" + (position + 1) + "个列表项");
+				// text.setTextSize(20);
+				// text.setTextColor(Color.RED);
+				// line.addView(image);
+				// line.addView(text);
+				// // 返回LinearLayout实例
+				// return line;
+				//
+				LayoutInflater inflater = getLayoutInflater();
+				View v = inflater.inflate(R.layout.baseadapter_item, null,
+						false);
+				ImageView image = (ImageView) v.findViewById(R.id.img);
+				image.setImageResource(R.drawable.libai);
+				TextView txt = (TextView) v.findViewById(R.id.txt);
+				txt.setText("hahhah");
+				return v;
 			}
 
 			// 重写该方法，该方法的返回值将作为列表项的ID
--- CommonComponent/src/com/common/ui/WeddingList.java
+++ CommonComponent/src/com/common/ui/WeddingList.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -32,6 +35,10 @@ public class WeddingList extends Activity {
 				LayoutInflater inflater = getLayoutInflater();
 				View itemview = inflater.inflate(
 						R.layout.simple_invitation_item, null);
+				// AbsListView.LayoutParams param = new
+				// AbsListView.LayoutParams(
+				// LinearLayout.LayoutParams.MATCH_PARENT, 150);
+				// itemview.setLayoutParams(param);
 				listid = (TextView) itemview.findViewById(R.id.listid);
 				listid.setText("第" + position + "张");
 				listimg = (ImageView) itemview.findViewById(R.id.listimg);
@@ -46,12 +53,24 @@ public class WeddingList extends Activity {
 				listdrink = (TextView) itemview.findViewById(R.id.listdrink);
 				listdrink.setText("新娘名");
 				return itemview;
+
+				// LayoutInflater inflater = getLayoutInflater();
+				// View v = inflater.inflate(R.layout.baseadapter_item, null);
+				// AbsListView.LayoutParams param = new
+				// AbsListView.LayoutParams(
+				// LinearLayout.LayoutParams.MATCH_PARENT, 150);
+				// v.setLayoutParams(param);
+				// ImageView image = (ImageView) v.findViewById(R.id.img);
+				// image.setImageResource(R.drawable.libai);
+				// TextView txt = (TextView) v.findViewById(R.id.txt);
+				// txt.setText("hahhah");
+				// return v;
 			}
 
 			@Override
 			public long getItemId(int position) {
 
-				return 0;
+				return position;
 			}
 
 			@Override
