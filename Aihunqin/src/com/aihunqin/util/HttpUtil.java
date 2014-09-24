79807f8555fae1226b313879f6df1eec7789d71b
daishuguang <daishuguang4461@126.com>
2014-09-25 00:49:47 +0800

    commit

--- Aihunqin/src/com/aihunqin/util/HttpUtil.java
+++ Aihunqin/src/com/aihunqin/util/HttpUtil.java
@@ -35,8 +35,8 @@ public class HttpUtil {
 	// public static HttpClient httpClient = new DefaultHttpClient();
 	public static DefaultHttpClient httpClient = new DefaultHttpClient();
 
-	public static final String BASE_URL = "http://www.ryapp.cn:3083";
-
+	//public static final String BASE_URL = "http://www.ryapp.cn:3083";
+	public static final String BASE_URL = "http://home.ihunqin.com";
 	public static String getRequest(final String url) throws Exception {
 		// 创建HttpGet对象.
 		HttpGet get = new HttpGet(url);
