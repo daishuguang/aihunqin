1620db18e0e2cb2e426295cf5060d686352e9dab
daishuguang <daishuguang4461@126.com>
2014-10-25 19:08:13 +0800

    commit

---
+++ Aihunqin/src/com/ihunqin/crazy/ContactsFragment.java
@@ -0,0 +1,111 @@
+package com.ihunqin.crazy;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract.Contacts;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.CursorAdapter;
+
+import com.example.aihunqin.R;
+
+public class ContactsFragment extends Fragment implements
+		LoaderManager.LoaderCallbacks<Cursor>, AdapterView.OnItemClickListener {
+	/*
+	 * Defines an array that contains column names to move from the Cursor to
+	 * the ListView.
+	 */
+
+	@SuppressWarnings("InlineApi")
+	private final static String[] FROM_COLUMNS = { Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ? Contacts.DISPLAY_NAME_PRIMARY
+			: Contacts.DISPLAY_NAME };
+
+	/*
+	 * Defines an array that contains resource ids for the layout views that get
+	 * the Cursor column contents. The id is pre-defined in the Android
+	 * framework, so it is prefaced with "android.R.id"
+	 */
+	private final static int[] TO_IDS = { android.R.id.text1 };
+
+	// Define global mutable variables
+	// Define a ListView object
+	ListView mContactsList;
+	// Define variables for the contact the user selects
+	// The contact's _ID value
+	long mContactId;
+
+	// The contact's LOOKUP_KEY
+	String mContactKey;
+
+	// A content URI for the selected contact
+	Uri mContactUri;
+
+	// An adapter that binds the result Cursor to the ListView
+	private SimpleCursorAdapter mCursorAdapter;
+
+	// The column index for the _ID column
+	private static final int CONTACT_ID_INDEX = 0;
+	// The column index for the LOOKUP_KEY column
+	private static final int LOOKUP_KEY_INDEX = 1;
+
+	// Empty public constructor, required by the system
+	public ContactsFragment() {
+	}
+
+	// A UI Fragment must inflate its View
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the fragment layout
+		return inflater.inflate(R.layout.contacts_list_view, container, false);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+
+		super.onActivityCreated(savedInstanceState);
+		// Gets the ListView from the View list of the parent activity
+		mContactsList = (ListView) getActivity().findViewById(
+				R.layout.contacts_list_view);
+		// Set the item click listener to be the current fragment.
+		mContactsList.setOnItemClickListener(this);
+		// Gets a CursorAdapter
+		mCursorAdapter = new SimpleCursorAdapter(getActivity(),
+				R.layout.contacts_list_item, null, FROM_COLUMNS, TO_IDS, 0);
+		// Sets the adapter for the ListView
+		mContactsList.setAdapter(mCursorAdapter);
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+		return null;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		// Get the Cursor
+		Cursor cursor = ((CursorAdapter) parent.getAdapter()).getCursor();
+		// Move to the selected contact
+		cursor.moveToPosition(position);
+
+	}
+}
