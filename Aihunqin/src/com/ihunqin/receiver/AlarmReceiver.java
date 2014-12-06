e01858987fb4d61ab1b90a99b3d438d420836379
Alex <daishuguang4461@126.com>
2014-12-06 13:56:38 +0800

    commit

--- Aihunqin/src/com/ihunqin/receiver/AlarmReceiver.java
+++ Aihunqin/src/com/ihunqin/receiver/AlarmReceiver.java
@@ -1,16 +1,23 @@
 package com.ihunqin.receiver;
 
-import com.ihunqin.activity.AlarmActivity;
+import com.example.aihunqin.R;
+import com.ihunqin.activity.SplashActivity;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Vibrator;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
 	Vibrator vibrator;
+	NotificationManager nm;
+	static final int NOTIFICATION_ID = 0x123;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
@@ -19,11 +26,43 @@ public class AlarmReceiver extends BroadcastReceiver {
 		// context.startActivity(i);
 
 		String msg = intent.getStringExtra("msg");
-		Toast.makeText(context, "婚礼任务提醒:" + msg, Toast.LENGTH_LONG).show();
+		// Toast.makeText(context, "婚礼任务提醒:" + msg, Toast.LENGTH_LONG).show();
+		nm = (NotificationManager) context
+				.getSystemService(context.NOTIFICATION_SERVICE);
+		// Notification notify = new NotificationCompat.Builder(context)
+		// .setAutoCancel(true).setContentTitle(msg)
+		// .setDefaults(Notification.DEFAULT_VIBRATE).build();
+		// nm.notify(0, notify);
+
+		/**
+		 * 
+		 */
+
+		Intent intent2 = new Intent(context, SplashActivity.class);
+
+		PendingIntent pi = PendingIntent.getActivity(context, 0, intent2, 0);
+
+		Notification notify = new Notification(R.drawable.hunqin, msg,
+				System.currentTimeMillis());
+		notify.setLatestEventInfo(context, "婚庆助手", msg, pi);
+		long[] vibrate = new long[] { 1000, 1000, 1000, 1000 };
+		notify.vibrate = vibrate;
+
+		nm.notify(NOTIFICATION_ID, notify);
+
+		//
+		// NotificationCompat.Builder builder = new NotificationCompat.Builder(
+		// context);
+		// builder.setDefaults(Notification.DEFAULT_VIBRATE).setAutoCancel(true)
+		// .setContentText(msg).setContentTitle(msg);
+		//
+		// nm.notify(1000, builder.build());
+
 		vibrator = (Vibrator) context
 				.getSystemService(Service.VIBRATOR_SERVICE);
 		vibrator.vibrate(5000);
-		// Toast.makeText(context, "婚礼任务提醒", Toast.LENGTH_LONG).show();
+		Toast.makeText(context, "婚礼任务提醒", Toast.LENGTH_LONG).show();
+
 	}
 
 }
---
+++ CommonComponent/src/com/common/appinteract/NotificationTest.java
@@ -0,0 +1,58 @@
+package com.common.appinteract;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.example.commoncomponent.R;
+
+public class NotificationTest extends Activity {
+
+	static final int NOTIFICATION_ID = 0x123;
+	NotificationManager nm;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.notificationlayout);
+		nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+		Button b1 = (Button) findViewById(R.id.send);
+		b1.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// Notification notify = new NotificationCompat.Builder(
+				// getApplicationContext()).setAutoCancel(true)
+				// .setTicker("有新消息").setSmallIcon(R.drawable.ic_launcher)
+				// .setContentTitle("一条新通知")
+				// .setContentText("恭喜你，您加薪了，工资增加")
+				// .setWhen(System.currentTimeMillis())
+				// .setVibrate(new long[] { 0, 50, 100, 150 }).build();
+
+				Intent intent = new Intent(NotificationTest.this,
+						NotificationTest.class);
+
+				PendingIntent pi = PendingIntent.getActivity(
+						NotificationTest.this, 0, intent, 0);
+
+				Notification notify = new Notification(R.drawable.ic_launcher,
+						"Notification", System.currentTimeMillis());
+				notify.setLatestEventInfo(getApplicationContext(),
+						"contentTitle", "contentText", pi);
+				long[] vibrate = new long[] { 1000, 1000, 1000, 1000 };
+				notify.vibrate = vibrate;
+
+				nm.notify(NOTIFICATION_ID, notify);
+			}
+		});
+	}
+}
